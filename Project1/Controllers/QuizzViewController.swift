337c29ad3a0412d10742af8ff4510ae6d6ba46f6
Sean Egger <eggersean@gmail.com>
2018-02-16 03:31:33 -0500

    fixed the quiz mostly
    changed quiz to use underscore labels

--- Project1/Controllers/QuizzViewController.swift
+++ Project1/Controllers/QuizzViewController.swift
@@ -18,14 +18,15 @@ class QuizzViewController: UIViewController {
     var numStrikes: Int = 0
     var score: Int = 0
     var answer:String = ""
-    var incompleteAnswer: String = ""
+    var correctGuesses: Int = 0
     var hearts: [UIImageView] = []
+    var letterSpots: [UILabel] = []
+    var customKeybard: [UIButton] = []
     
     
     //MARK: Outlets
     @IBOutlet weak var emoji: UILabel!
     
-    @IBOutlet weak var answerLabel: UILabel!
     
     @IBOutlet weak var scoreLabel: UILabel!
     
@@ -36,15 +37,106 @@ class QuizzViewController: UIViewController {
     //MARK: Custom Keyboard
     @IBAction func customKeyBoardPress(_ sender: UIButton) {
         sender.isHidden = true
-        guessLetter(button: sender.currentTitle!)
+        guessLetter(letter: sender.currentTitle!.lowercased())
     }
+    
+    @IBOutlet weak var keyA: UIButton!
+    @IBOutlet weak var keyB: UIButton!
+    @IBOutlet weak var keyC: UIButton!
+    @IBOutlet weak var keyD: UIButton!
+    @IBOutlet weak var keyE: UIButton!
+    @IBOutlet weak var keyF: UIButton!
+    @IBOutlet weak var keyG: UIButton!
+    @IBOutlet weak var keyH: UIButton!
+    @IBOutlet weak var keyI: UIButton!
+    @IBOutlet weak var keyJ: UIButton!
+    @IBOutlet weak var keyK: UIButton!
+    @IBOutlet weak var keyL: UIButton!
+    @IBOutlet weak var keyM: UIButton!
+    @IBOutlet weak var keyN: UIButton!
+    @IBOutlet weak var keyO: UIButton!
+    @IBOutlet weak var keyP: UIButton!
+    @IBOutlet weak var keyQ: UIButton!
+    @IBOutlet weak var keyR: UIButton!
+    @IBOutlet weak var keyS: UIButton!
+    @IBOutlet weak var keyT: UIButton!
+    @IBOutlet weak var keyU: UIButton!
+    @IBOutlet weak var keyV: UIButton!
+    @IBOutlet weak var keyW: UIButton!
+    @IBOutlet weak var keyX: UIButton!
+    @IBOutlet weak var keyY: UIButton!
+    @IBOutlet weak var keyZ: UIButton!
+    
+    
+    //MARK: HiddenWord
+    @IBOutlet weak var spot1: UILabel!
+    @IBOutlet weak var spot2: UILabel!
+    @IBOutlet weak var spot3: UILabel!
+    @IBOutlet weak var spot4: UILabel!
+    @IBOutlet weak var spot5: UILabel!
+    @IBOutlet weak var spot6: UILabel!
+    @IBOutlet weak var spot7: UILabel!
+    @IBOutlet weak var spot8: UILabel!
+    @IBOutlet weak var spot9: UILabel!
+    @IBOutlet weak var spot10: UILabel!
+    @IBOutlet weak var spot11: UILabel!
+    @IBOutlet weak var spot12: UILabel!
+    @IBOutlet weak var spot13: UILabel!
+    @IBOutlet weak var spot14: UILabel!
+    @IBOutlet weak var spot15: UILabel!
+    
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        emoji.text = ""
         // Do any additional setup after loading the view.
+        //set emoji text to be blank
+        emoji.text = ""
+        //load letter spots into array
+        letterSpots.append(spot1)
+        letterSpots.append(spot2)
+        letterSpots.append(spot3)
+        letterSpots.append(spot4)
+        letterSpots.append(spot5)
+        letterSpots.append(spot6)
+        letterSpots.append(spot7)
+        letterSpots.append(spot8)
+        letterSpots.append(spot9)
+        letterSpots.append(spot10)
+        letterSpots.append(spot11)
+        letterSpots.append(spot12)
+        letterSpots.append(spot13)
+        letterSpots.append(spot14)
+        letterSpots.append(spot15)
+        //load keyboard into array
+        customKeybard.append(keyA)
+        customKeybard.append(keyB)
+        customKeybard.append(keyC)
+        customKeybard.append(keyD)
+        customKeybard.append(keyE)
+        customKeybard.append(keyF)
+        customKeybard.append(keyG)
+        customKeybard.append(keyH)
+        customKeybard.append(keyI)
+        customKeybard.append(keyJ)
+        customKeybard.append(keyK)
+        customKeybard.append(keyL)
+        customKeybard.append(keyM)
+        customKeybard.append(keyN)
+        customKeybard.append(keyO)
+        customKeybard.append(keyP)
+        customKeybard.append(keyQ)
+        customKeybard.append(keyR)
+        customKeybard.append(keyS)
+        customKeybard.append(keyT)
+        customKeybard.append(keyU)
+        customKeybard.append(keyV)
+        customKeybard.append(keyW)
+        customKeybard.append(keyX)
+        customKeybard.append(keyY)
+        customKeybard.append(keyZ)
         //create hearts list
         hearts.append(heart1)
         hearts.append(heart2)
@@ -77,42 +169,22 @@ class QuizzViewController: UIViewController {
      not in the answer then the user gets a strike. Checks for win/loss
      conditions
     */
-    func guessLetter(button: String)
+    func guessLetter(letter: String)
     {
         //check if its in the answer
-        let letter = Array(button)[0]
-        if answer.contains(letter)
+        if answer.lowercased().contains(letter)
         {
-            //if it is fill in the characters
-            let answerChars = Array(answer)
-            var incompleteAnswerChars = Array(incompleteAnswer)
-            for (index,char) in answerChars.enumerated()
+            //reveal all guessed letters in word
+            for (index, character) in answer.lowercased().enumerated()
             {
-                if (char == letter)
+                if (Array(letter)[0] == character)
                 {
-                    // reveal that letter in the incomplete answer
-                    print("Correct Letter Clicked")
-                    print(letter)
-                    incompleteAnswerChars[index] = letter
+                    letterSpots[index].isHidden = false
+                    letterSpots[index].text = String(character)
+                    correctGuesses += 1
                 }
             }
-            incompleteAnswer = String(incompleteAnswerChars)
-            //check if word is complete
-            if(incompleteAnswer == answer)
-            {
-                //increment score
-                score += 1
-                //is that the last question
-                if(currentQuestionIndex == numQuestions - 1)
-                {
-                    gameOver()
-                }
-                //if not move onto next question
-                else
-                {
-                    nextQuestion()
-                }
-            }
+            checkForCompletion()
         }
         //letter is not in answer
         else
@@ -122,6 +194,31 @@ class QuizzViewController: UIViewController {
             if (numStrikes == 3)
             {
                 gameOver()
+            }
+        }
+    }
+    
+    /*
+     Called when player completes a word. Checks if word is final word
+     or if the next word should be displayed
+    */
+    func checkForCompletion()
+    {
+        //check if word is complete
+        if(correctGuesses == answer.count)
+        {
+            //increment score
+            score += 1
+            scoreLabel.text = String(score)
+            //is that the last question
+            if(currentQuestionIndex == numQuestions - 1)
+            {
+                gameOver()
+            }
+                //if not move onto next question
+            else
+            {
+                nextQuestion()
             }
         }
     }
@@ -136,26 +233,40 @@ class QuizzViewController: UIViewController {
     {
         //increment index
         currentQuestionIndex += 1
+        //set number of correct guesses back to zero
+        correctGuesses = 0
         //animate the change of emoji
         emoji.text = emojiQuestionAnswer[currentQuestionIndex].question
         emoji.alpha = 0;
         [UIView .animate(withDuration: 0.5, delay: 0, options: UIViewAnimationOptions.curveEaseIn, animations:{self.emoji.alpha=1;}, completion: nil)];
 
-       
         //set the new answer
         answer = emojiQuestionAnswer[currentQuestionIndex].answer
-        for character in Array(answer)
+        // set up the empty letter spot labels
+        for (index, character) in (Array(answer).enumerated())
         {
-            if (character == " ")
+            //if its a space make it hidden. If not set it to _
+            print("character: ", character)
+            if(character == " ")
             {
-                incompleteAnswer += " "
+                letterSpots[index].isHidden = true
             }
             else
             {
-                incompleteAnswer += "_"
+                letterSpots[index].isHidden = false
+                letterSpots[index].text = "_"
             }
         }
-        answerLabel.text = incompleteAnswer
+        // hide the extra labels
+        for labelIndex in Array(answer).count...letterSpots.count - 1
+        {
+            letterSpots[labelIndex].isHidden = true
+        }
+        //show the keyboard
+        for key in customKeybard
+        {
+            key.isHidden = false
+        }
     }
     
     
